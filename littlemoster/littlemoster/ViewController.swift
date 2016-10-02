dfe12d06ddc46c2c76f95b984cb9e03b87a429d3
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-10-03 01:26:31 +0530

    updated the UI

--- littlemoster/littlemoster/ViewController.swift
+++ littlemoster/littlemoster/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imgArray = [UIImage]()
+            for var x = 1; x <= 4; x++ {
+                var img = UIImage(named: "idle\(x).png")
+                imgArray.append(img!)
+            }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
+        
     
     }
 
