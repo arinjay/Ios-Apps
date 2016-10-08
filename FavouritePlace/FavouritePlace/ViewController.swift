3798c9608a5d9543c00c80a897dc02c4da75dfcc
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-10-08 17:13:11 +0530

    Adding UI

--- FavouritePlace/FavouritePlace/ViewController.swift
+++ FavouritePlace/FavouritePlace/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var moreButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moreButton.layer.cornerRadius = 2.0
     }
 
     override func didReceiveMemoryWarning() {
