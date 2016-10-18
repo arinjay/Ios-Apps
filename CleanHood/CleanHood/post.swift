adfe6611ac360c041a8b0222eecb663524111cfc
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-10-18 21:01:57 +0530

    Initial Commit

---
+++ CleanHood/CleanHood/post.swift
@@ -0,0 +1,27 @@
+//
+//  post.swift
+//  CleanHood
+//
+//  Created by Arinjay on 18/10/16.
+//  Copyright © 2016 Arinjay. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var imagePath: String
+    private var title: String
+    private var PostDesc: String
+    
+    init(imagePath: String, title: String, decription: String){
+        
+        self.imagePath = imagePath
+        self.title = title
+        self.PostDesc = decription
+        
+    }
+    
+    
+    
+}
