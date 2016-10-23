f773f5ec993d4f6c54d0ecbab35b5fe424efc0ce
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-10-24 01:42:21 +0530

    Adding & linking entities of coreData

--- FavRecipe/FavRecipe/ViewController.swift
+++ FavRecipe/FavRecipe/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var recipes = [Recipe]()
+    
+    
 
     
     override func viewDidLoad() {
@@ -22,7 +25,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        fetchAndSetResult()
+        tableView.reloadData()
+    }
 
+    func fetchAndSetResult(){
+        
+        let app = UIApplication.sharedApplication().delegate as! AppDelegate  // give you main delegate
+        let context = app.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Recipe")
+        do{
+            let results = try context.executeFetchRequest(fetchRequest)
+            self.recipes = results as! [Recipe]
+            
+        }
+        catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //return UITableViewCell()
