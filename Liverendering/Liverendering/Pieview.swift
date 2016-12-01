502c2ebf455265c589db200394df74b0fd8bad08
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-12-02 03:05:47 +0530

    adding first layer to pie chart

--- Liverendering/Liverendering/Pieview.swift
+++ Liverendering/Liverendering/Pieview.swift
@@ -48,10 +48,57 @@ class Pieview: UIView {
     }
     
     func createChart(){
+        layoutBackgroundLayer()
+        layoutBackgroundImageLayer()
+    }
+    
+    func layoutBackgroundLayer (){
+        if backgroundLayer == nil {
+            backgroundLayer = CAShapeLayer()
+            layer.addSublayer(backgroundLayer)
+        
+            let rectangle = bounds.insetBy(dx: lineWidth / 2, dy: lineWidth / 2)
+            let path = UIBezierPath(ovalIn: rectangle)
+            
+            backgroundLayer.path = path.cgPath
+            backgroundLayer.fillColor = backgroundColor?.cgColor
+            backgroundLayer.lineWidth = lineWidth
+            
+            backgroundLayer.frame = layer.bounds
+            
+            
+        }
         
     }
     
+    func layoutBackgroundImageLayer (){
+        if backgroundImageLayer == nil {
+            let imageMask = CAShapeLayer()
+            let inset = lineWidth + 3
+            let insetBounds = self.bounds
+            let maskPath = UIBezierPath(ovalIn: insetBounds)
+            imageMask.path = maskPath.cgPath
+            imageMask.fillColor = UIColor.black.cgColor
+            imageMask.frame = self.bounds
+            
+            
+            backgroundImageLayer = CAShapeLayer()
+            backgroundImageLayer.mask = imageMask
+            backgroundImageLayer.frame = self.bounds
+            backgroundImageLayer.contentsGravity = kCAGravityResizeAspectFill
+            layer.addSublayer(backgroundImageLayer)
+            
+            
+        }
+    }
+    
     func updateLayerproperties() {
+        
+        if backgroundImageLayer != nil {
+            if let image = backgroundImage {
+                backgroundImageLayer.contents = image.cgImage
+            }
+        }
         
     }
     
