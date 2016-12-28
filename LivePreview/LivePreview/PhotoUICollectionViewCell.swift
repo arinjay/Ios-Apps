622c3c10d7ce3f0c6e3201bb74399a21e29934b2
Arinjay Sharma <arinjay@users.noreply.github.com>
2016-12-28 21:02:09 +0530

    Adding basic framework

---
+++ LivePreview/LivePreview/PhotoUICollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoUICollectionViewCell.swift
+//  LivePreview
+//
+//  Created by Arinjay on 28/12/16.
+//  Copyright © 2016 Arinjay. All rights reserved.
+//
+
+import UIKit
+
+class PhotoUICollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
