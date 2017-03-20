6773022005f3edf899098dddfd3c793e3ac81172
Arinjay Sharma <arinjay@users.noreply.github.com>
2017-03-20 23:08:04 +0530

    First commit

---
+++ Polyglot/Polyglot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polyglot
+//
+//  Created by Arinjay Sharma on 3/19/17.
+//  Copyright © 2017 Arinjay Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
