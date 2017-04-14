73e8e55e3aab7ca98ea5cf2ed5d68c9ab1cb12fe
Arinjay Sharma <arinjay@users.noreply.github.com>
2017-04-14 12:43:40 +0530

    added scoring functionality

--- Pong/Pong/GameScene.swift
+++ Pong/Pong/GameScene.swift
@@ -11,12 +11,17 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    
+    
     var ball = SKSpriteNode()
     var enemy = SKSpriteNode()
     var main = SKSpriteNode()
+    var score = [Int]()
     
     
     override func didMove(to view: SKView) {
+        
+        startGame()
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
@@ -32,6 +37,30 @@ class GameScene: SKScene {
         border.restitution = 1
         self.physicsBody = border
     }
+    
+    
+    func startGame(){
+        
+        score = [0,0]
+    }
+    
+    func addScore(playerwhoWon: SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        //ball.physicsBody?.velocity = CGVector(dx: 10, dy: 10)
+        
+        if playerwhoWon == main {
+            score[0] += 1
+              ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy:20))
+        }
+        else if playerwhoWon == enemy {
+            score[1] += 1
+              ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        print(score)
+    }
+    
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
      
@@ -59,5 +88,17 @@ class GameScene: SKScene {
         
         //moving enemy
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= main.position.y - 70 {
+            addScore(playerwhoWon: enemy)
+            
+        }
+        else if ball.position.y >= main.position.y + 70 {
+            addScore(playerwhoWon: main)
+        }
+        
+        
+        
+        
     }
 }
